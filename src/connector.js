0a9b41083d0a0c72db4dbf099de117e1cae71010
Paul Proell <paul.proell@hotmail.com>
2023-08-28 08:39:58 +0200

    fix

--- src/connector.js
+++ src/connector.js
@@ -319,7 +319,7 @@ class BotiumConnectorVoip {
             if (this.silenceTimeout) {
               clearTimeout(this.silenceTimeout)
             }
-            if (!this.firstSttInfoReceived && this._getJoinLogicHook(this.convoStep) && this.caps[Capabilities.VOIP_SILENCE_DURATION_TIMEOUT_START_ENABLE] && parsedData.data.start && parsedData.data.start * 1000 > this.caps[Capabilities.VOIP_SILENCE_DURATION_TIMEOUT_START]) {
+            if (!this.firstSttInfoReceived && _.isNil(this._getJoinLogicHook(this.convoStep)) && this.caps[Capabilities.VOIP_SILENCE_DURATION_TIMEOUT_START_ENABLE] && parsedData.data.start && parsedData.data.start * 1000 > this.caps[Capabilities.VOIP_SILENCE_DURATION_TIMEOUT_START]) {
               this.end = true
               sendBotMsg(new Error(`Silence Duration of ${parsedData.data.start}s exceeded Initial Silence Duration Timeout of ${this.caps[Capabilities.VOIP_SILENCE_DURATION_TIMEOUT_START] / 1000}s`))
             }
